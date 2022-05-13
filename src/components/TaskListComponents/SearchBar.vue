6660e85f77f340c66fbe8415c41afb2b0955620d
王珠峰 <734765725@qq.com>
2022-05-13 17:51:23 +0800

    路由配置

---
+++ src/components/TaskListComponents/SearchBar.vue
@@ -0,0 +1,11 @@
+<template>
+    SearchBar
+</template>
+  
+<script setup lang='ts'>
+  
+</script>
+  
+<style>
+  
+</style>
---
+++ src/views/TaskList.vue
@@ -0,0 +1,12 @@
+<template>
+    <h4>TaskList </h4> 
+    <SearchBar></SearchBar>
+</template>
+  
+<script setup lang='ts'>
+import SearchBar from '../components/TaskListComponents/SearchBar.vue'
+
+</script>
+  
+<style>
+</style>
